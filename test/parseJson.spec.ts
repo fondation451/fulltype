f1e3c4966acff4d10e0b40e1198001a269f74ff1
Nicolas Assouad <nicolas@assouad.com>
2021-01-27 18:14:08 +0100

    all good case

--- test/parseJson.spec.ts
+++ test/parseJson.spec.ts
@@ -17,6 +17,7 @@ describe('parseJson', () => {
         const _typeCheck: number = parsedJson;
         expect(parsedJson).toEqual(JSON.parse(json));
       });
+
       it('should parse a valid JSON (case boolean)', () => {
         const model = buildModel({
           kind: 'primitive',
@@ -30,6 +31,7 @@ describe('parseJson', () => {
         const _typeCheck: boolean = parsedJson;
         expect(parsedJson).toEqual(JSON.parse(json));
       });
+
       it('should parse a valid JSON (case string)', () => {
         const model = buildModel({
           kind: 'primitive',
@@ -43,6 +45,7 @@ describe('parseJson', () => {
         const _typeCheck: string = parsedJson;
         expect(parsedJson).toEqual(JSON.parse(json));
       });
+
       it('should parse a valid JSON (case date)', () => {
         const model = buildModel({
           kind: 'primitive',
@@ -57,62 +60,136 @@ describe('parseJson', () => {
         expect(parsedJson.toISOString()).toEqual(JSON.parse(json));
       });
     });
-    describe('constant case', () => {
-      it('should parse a valid JSON (constant)', () => {
-        const model = buildModel({
-          kind: 'constant',
-          content: ['CONSTANT_1', 'CONSTANT_2'],
-          exclude: [],
-        } as const);
-        const json = `"CONSTANT_2"`;
 
-        const parsedJson = parseJson(model, json);
+    it('should parse a valid JSON (constant case)', () => {
+      const model = buildModel({
+        kind: 'constant',
+        content: ['CONSTANT_1', 'CONSTANT_2'],
+        exclude: [],
+      } as const);
+      const json = `"CONSTANT_2"`;
 
-        const _typeCheck: 'CONSTANT_1' | 'CONSTANT_2' = parsedJson;
-        expect(parsedJson).toEqual(JSON.parse(json));
-      });
+      const parsedJson = parseJson(model, json);
+
+      const _typeCheck: 'CONSTANT_1' | 'CONSTANT_2' = parsedJson;
+      expect(parsedJson).toEqual(JSON.parse(json));
     });
-    describe('array case', () => {
-      it('should parse a valid JSON (array)', () => {
-        const model = buildModel({
-          kind: 'array',
-          content: {
-            kind: 'primitive',
-            content: 'boolean',
-            exclude: [],
-          },
+
+    it('should parse a valid JSON (array case)', () => {
+      const model = buildModel({
+        kind: 'array',
+        content: {
+          kind: 'primitive',
+          content: 'boolean',
           exclude: [],
-        } as const);
-        const json = `[true, false, true]`;
+        },
+        exclude: [],
+      } as const);
+      const json = `[true, false, true]`;
 
-        const parsedJson = parseJson(model, json);
+      const parsedJson = parseJson(model, json);
 
-        const _typeCheck: boolean[] = parsedJson;
-        expect(parsedJson).toEqual(JSON.parse(json));
-      });
+      const _typeCheck: boolean[] = parsedJson;
+      expect(parsedJson).toEqual(JSON.parse(json));
     });
-    describe('object case', () => {
-      it('should parse a valid JSON (object)', () => {
-        const model = buildModel({
-          kind: 'object',
-          content: {
-            field1: { kind: 'primitive', content: 'boolean', exclude: [] },
-            field2: { kind: 'primitive', content: 'number', exclude: [] },
-            field3: { kind: 'primitive', content: 'string', exclude: [] },
-          },
-          exclude: [],
-        } as const);
-        const json = `{
+
+    it('should parse a valid JSON (object case)', () => {
+      const model = buildModel({
+        kind: 'object',
+        content: {
+          field1: { kind: 'primitive', content: 'boolean', exclude: [] },
+          field2: { kind: 'primitive', content: 'number', exclude: [] },
+          field3: { kind: 'primitive', content: 'string', exclude: [] },
+        },
+        exclude: [],
+      } as const);
+      const json = `{
           "field1": false,
           "field2": 1,
           "field3": "STRING"
         }`;
 
-        const parsedJson = parseJson(model, json);
+      const parsedJson = parseJson(model, json);
 
-        const _typeCheck: { field1: boolean; field2: number; field3: string } = parsedJson;
-        expect(parsedJson).toEqual(JSON.parse(json));
-      });
+      const _typeCheck: { field1: boolean; field2: number; field3: string } = parsedJson;
+      expect(parsedJson).toEqual(JSON.parse(json));
+    });
+
+    it('should parse a valid JSON (general case)', () => {
+      const model = buildModel({
+        kind: 'object',
+        content: {
+          field1: { kind: 'primitive', content: 'boolean', exclude: [] },
+          field2: {
+            kind: 'array',
+            content: {
+              kind: 'object',
+              content: {
+                arraySubField1: {
+                  kind: 'primitive',
+                  content: 'string',
+                  exclude: [],
+                },
+                arraySubField2: {
+                  kind: 'array',
+                  content: {
+                    kind: 'primitive',
+                    content: 'boolean',
+                    exclude: [],
+                  },
+                  exclude: [],
+                },
+                arraySubField3: {
+                  kind: 'constant',
+                  content: ['CONSTANT1', 'CONSTANT2'],
+                  exclude: [],
+                },
+              },
+              exclude: [],
+            },
+            exclude: [],
+          },
+          field3: {
+            kind: 'array',
+            content: {
+              kind: 'constant',
+              content: ['ANOTHER_CONSTANT1', 'ANOTHER_CONSTANT2'],
+              exclude: [],
+            },
+            exclude: [],
+          },
+        },
+        exclude: [],
+      } as const);
+      const json = `{
+        "field1": false,
+        "field2": [
+          {
+            "arraySubField1": "STRING",
+            "arraySubField2": [true, false, true],
+            "arraySubField3": "CONSTANT2"
+          },
+          {
+            "arraySubField1": "ANOTHER_STRING",
+            "arraySubField2": [false, false],
+            "arraySubField3": "CONSTANT1"
+          }
+        ],
+        "field3": ["ANOTHER_CONSTANT1", "ANOTHER_CONSTANT1", "ANOTHER_CONSTANT2"]
+      }`;
+
+      const parsedJson = parseJson(model, json);
+
+      const _typeCheck: {
+        field1: boolean;
+        field2: Array<{
+          arraySubField1: string;
+          arraySubField2: Array<boolean>;
+          arraySubField3: 'CONSTANT1' | 'CONSTANT2';
+        }>;
+        field3: Array<'ANOTHER_CONSTANT1' | 'ANOTHER_CONSTANT2'>;
+      } = parsedJson;
+      expect(parsedJson).toEqual(JSON.parse(json));
     });
   });
 });

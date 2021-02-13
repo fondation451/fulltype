c56d8bda33bfeed0520827c9a802e6eae5edf1df
Nicolas Assouad <nicolas@assouad.com>
2021-02-13 11:36:20 +0100

    new generate function

---
+++ src/model/generate.ts
@@ -0,0 +1,128 @@
+import { buildType, buildPrimitiveType, customMappingType } from './buildType';
+import { modelType, modelPrimitiveType, modelConstantType, modelObjectType } from './modelType';
+
+export { generate };
+
+const MAX_NUMBER = 100000000;
+const MAX_ARRAY_LENGTH = 5;
+
+function generate<modelT extends modelType, customMappingT extends customMappingType>({
+  model,
+  customGenerator = {} as { [key in keyof customMappingT]: (randomInt: number) => customMappingT[key] },
+}: {
+  model: modelT;
+  customGenerator?: { [key in keyof customMappingT]: (randomInt: number) => customMappingT[key] };
+}): buildType<modelT, customMappingT> {
+  return generateFromModel({ model, customGenerator });
+}
+
+function generateFromModel<modelT extends modelType, customMappingT extends customMappingType>({
+  model,
+  customGenerator,
+  currentFieldName,
+}: {
+  model: modelT;
+  customGenerator: { [key in keyof customMappingT]: (randomInt: number) => customMappingT[key] };
+  currentFieldName?: string;
+}): buildType<modelT, customMappingT> {
+  switch (model.kind) {
+    case 'primitive':
+      type modelPrimitiveT = modelT['content'] extends modelPrimitiveType ? modelT['content'] : any;
+      return generateFromPrimitive({ modelPrimitive: model.content as modelPrimitiveT, currentFieldName }) as buildType<
+        modelT,
+        customMappingT
+      >;
+    case 'constant':
+      type modelConstantT = modelT['content'] extends modelConstantType ? modelT['content'] : any;
+      const constants = model.content as modelConstantT;
+
+      return constants[random(constants.length)] as buildType<modelT, customMappingT>;
+    case 'custom':
+      return customGenerator[model.content as string](random(MAX_NUMBER)) as buildType<modelT, customMappingT>;
+    case 'object':
+      type modelObjectT = modelT['content'] extends modelObjectType ? modelT['content'] : any;
+      return generateFromObject({
+        modelObject: model.content as modelObjectT,
+        customGenerator,
+        currentFieldName,
+      }) as buildType<modelT, customMappingT>;
+    case 'array':
+      type modelArrayItemT = modelT['content'] extends modelType ? modelT['content'] : any;
+      const generatedArray = [] as buildType<modelT, customMappingT>;
+      const generatedArrayLength = random(MAX_ARRAY_LENGTH);
+
+      for (let index = 0; index < generatedArrayLength; index++) {
+        generatedArray.push(
+          generateFromModel({
+            model: model.content as modelArrayItemT,
+            customGenerator,
+            currentFieldName: currentFieldName ? `${currentFieldName}_${index}` : undefined,
+          }),
+        );
+      }
+
+      return generatedArray;
+  }
+}
+
+function generateFromPrimitive<modelPrimitiveT extends modelPrimitiveType>({
+  modelPrimitive,
+  currentFieldName,
+}: {
+  modelPrimitive: modelPrimitiveT;
+  currentFieldName?: string;
+}): buildPrimitiveType<modelPrimitiveT> {
+  switch (modelPrimitive) {
+    case 'boolean':
+      return (random(2) === 0 ? true : false) as buildPrimitiveType<modelPrimitiveT>;
+    case 'date':
+      const day = random(28);
+      const month = random(13);
+      const year = 1970 + random(new Date(Date.now()).getFullYear() - 1970);
+
+      const date = new Date();
+      date.setDate(day);
+      date.setMonth(month);
+      date.setFullYear(year);
+
+      return date as buildPrimitiveType<modelPrimitiveT>;
+    case 'number':
+      return random(MAX_NUMBER) as buildPrimitiveType<modelPrimitiveT>;
+    case 'string':
+      return (currentFieldName
+        ? `${currentFieldName}_${random(MAX_NUMBER)}`
+        : `STRING_${random(MAX_NUMBER)}`) as buildPrimitiveType<modelPrimitiveT>;
+    case 'void':
+      return undefined as buildPrimitiveType<modelPrimitiveT>;
+  }
+
+  return undefined as buildPrimitiveType<modelPrimitiveT>;
+}
+
+function generateFromObject<modelObjectT extends modelObjectType, customMappingT extends customMappingType>({
+  modelObject,
+  customGenerator,
+  currentFieldName,
+}: {
+  modelObject: modelObjectT;
+  customGenerator: { [key in keyof customMappingT]: (randomInt: number) => customMappingT[key] };
+  currentFieldName?: string;
+}): { [key in keyof modelObjectT]: buildType<modelObjectT[key], customMappingT> } {
+  const generatedObject = {} as {
+    [key in keyof modelObjectT]: buildType<modelObjectT[key], customMappingT>;
+  };
+
+  for (const key in modelObject) {
+    generatedObject[key] = generateFromModel<modelObjectT[typeof key], customMappingT>({
+      model: modelObject[key],
+      customGenerator,
+      currentFieldName: currentFieldName ? `${currentFieldName}_${key}` : key,
+    });
+  }
+
+  return generatedObject;
+}
+
+function random(max: number) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
---
+++ test/generate.spec.ts
@@ -0,0 +1,228 @@
+import { ObjectId } from 'mongodb';
+import { buildModel } from '../src/model/buildModel';
+import { generate } from '../src/model/generate';
+
+const TEST_ITERATION = 5;
+
+describe('generate', () => {
+  describe('primitive case', () => {
+    it('should generate a random number', () => {
+      executeTestSeveralTimes(() => {
+        const model = buildModel({
+          kind: 'primitive',
+          content: 'number',
+        } as const);
+
+        const generatedValue = generate({ model });
+
+        const _typeCheck: number = generatedValue;
+        expect(typeof generatedValue).toEqual('number');
+      });
+    });
+
+    it('should generate a random boolean', () => {
+      executeTestSeveralTimes(() => {
+        const model = buildModel({
+          kind: 'primitive',
+          content: 'boolean',
+        } as const);
+
+        const generatedValue = generate({ model });
+
+        const _typeCheck: boolean = generatedValue;
+        expect(typeof generatedValue).toEqual('boolean');
+      });
+    });
+
+    it('should generate a random string', () => {
+      executeTestSeveralTimes(() => {
+        const model = buildModel({
+          kind: 'primitive',
+          content: 'string',
+        } as const);
+
+        const generatedValue = generate({ model });
+
+        const _typeCheck: string = generatedValue;
+        expect(typeof generatedValue).toEqual('string');
+      });
+    });
+
+    it('should generate a random date', () => {
+      executeTestSeveralTimes(() => {
+        const model = buildModel({
+          kind: 'primitive',
+          content: 'date',
+        } as const);
+
+        const generatedValue = generate({ model });
+
+        const _typeCheck: Date = generatedValue;
+        expect(generatedValue instanceof Date).toEqual(true);
+      });
+    });
+  });
+
+  it('should generate a random constant', () => {
+    executeTestSeveralTimes(() => {
+      const model = buildModel({
+        kind: 'constant',
+        content: ['CONSTANT_1', 'CONSTANT_2'],
+      } as const);
+
+      const generatedValue = generate({ model });
+
+      const _typeCheck: 'CONSTANT_1' | 'CONSTANT_2' = generatedValue;
+      expect(typeof generatedValue).toEqual('string');
+      expect(['CONSTANT_1', 'CONSTANT_2'].includes(generatedValue)).toEqual(true);
+    });
+  });
+
+  describe('custom case', () => {
+    it('should generate a random custom string', () => {
+      executeTestSeveralTimes(() => {
+        const model = buildModel({
+          kind: 'custom',
+          content: 'CUSTOM_TYPE',
+        } as const);
+
+        const generatedValue = generate({
+          model,
+          customGenerator: { CUSTOM_TYPE: (randomValue) => `CUSTOM_STRING_${randomValue}` },
+        });
+
+        const _typeCheck: string = generatedValue;
+        expect(generatedValue.startsWith('CUSTOM_STRING_')).toEqual(true);
+      });
+    });
+
+    it('should generate a random custom object id', () => {
+      executeTestSeveralTimes(() => {
+        const model = buildModel({
+          kind: 'custom',
+          content: 'Mongo_ID',
+        } as const);
+
+        const generatedValue = generate({ model, customGenerator: { Mongo_ID: () => new ObjectId() } });
+
+        const _typeCheck: ObjectId = generatedValue;
+        expect(ObjectId.isValid(generatedValue)).toEqual(true);
+      });
+    });
+  });
+
+  it('should generate a random array', () => {
+    executeTestSeveralTimes(() => {
+      const model = buildModel({
+        kind: 'array',
+        content: {
+          kind: 'primitive',
+          content: 'boolean',
+        },
+      } as const);
+
+      const generatedValue = generate({ model });
+
+      const _typeCheck: boolean[] = generatedValue;
+      expect(Array.isArray(generatedValue)).toEqual(true);
+      generatedValue.forEach((value) => expect(typeof value).toEqual('boolean'));
+    });
+  });
+
+  it('should generate a random object', () => {
+    executeTestSeveralTimes(() => {
+      const model = buildModel({
+        kind: 'object',
+        content: {
+          field1: { kind: 'primitive', content: 'boolean' },
+          field2: { kind: 'primitive', content: 'number' },
+          field3: { kind: 'primitive', content: 'string' },
+        },
+      } as const);
+
+      const generatedValue = generate({ model });
+
+      const _typeCheck: { field1: boolean; field2: number; field3: string } = generatedValue;
+      expect(typeof generatedValue.field1).toEqual('boolean');
+      expect(typeof generatedValue.field2).toEqual('number');
+      expect(typeof generatedValue.field3).toEqual('string');
+      expect(generatedValue.field3.startsWith('field3_')).toEqual(true);
+    });
+  });
+
+  it('should generate a random data from model (general case)', () => {
+    executeTestSeveralTimes(() => {
+      const model = buildModel({
+        kind: 'object',
+        content: {
+          field1: { kind: 'primitive', content: 'boolean' },
+          field2: {
+            kind: 'array',
+            content: {
+              kind: 'object',
+              content: {
+                arraySubField1: {
+                  kind: 'primitive',
+                  content: 'string',
+                },
+                arraySubField2: {
+                  kind: 'array',
+                  content: {
+                    kind: 'primitive',
+                    content: 'boolean',
+                  },
+                },
+                arraySubField3: {
+                  kind: 'constant',
+                  content: ['CONSTANT1', 'CONSTANT2'],
+                },
+              },
+            },
+          },
+          field3: {
+            kind: 'array',
+            content: {
+              kind: 'constant',
+              content: ['ANOTHER_CONSTANT1', 'ANOTHER_CONSTANT2'],
+            },
+          },
+        },
+      } as const);
+
+      const generatedValue = generate({ model });
+
+      const _typeCheck: {
+        field1: boolean;
+        field2: Array<{
+          arraySubField1: string;
+          arraySubField2: Array<boolean>;
+          arraySubField3: 'CONSTANT1' | 'CONSTANT2';
+        }>;
+        field3: Array<'ANOTHER_CONSTANT1' | 'ANOTHER_CONSTANT2'>;
+      } = generatedValue;
+      expect(typeof generatedValue.field1).toEqual('boolean');
+      expect(Array.isArray(generatedValue.field2)).toEqual(true);
+      generatedValue.field2.forEach((value, index) => {
+        expect(typeof value.arraySubField1).toEqual('string');
+        expect(value.arraySubField1.startsWith(`field2_${index}_arraySubField1_`)).toEqual(true);
+        expect(Array.isArray(value.arraySubField2)).toEqual(true);
+        value.arraySubField2.forEach((subValue) => {
+          expect(typeof subValue).toEqual('boolean');
+        });
+        expect(typeof value.arraySubField3).toEqual('string');
+        expect(['CONSTANT1', 'CONSTANT2'].includes(value.arraySubField3)).toEqual(true);
+      });
+      expect(Array.isArray(generatedValue.field3)).toEqual(true);
+      generatedValue.field3.forEach((value) => {
+        expect(typeof value).toEqual('string');
+        expect(['ANOTHER_CONSTANT1', 'ANOTHER_CONSTANT2'].includes(value)).toEqual(true);
+      });
+    });
+  });
+});
+
+function executeTestSeveralTimes(test: () => void) {
+  for (let i = 0; i < TEST_ITERATION; i++) {
+    test();
+  }
+}

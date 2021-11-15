cb019f9b8ede3ed9050d981a95b313af48c5e497
Nicolas Assouad <nicolas@assouad.com>
2021-11-15 18:44:08 +0100

    update lib interface

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
-export * from './typeArchitect';
-export * from './types';
+import { typeArchitect } from './typeArchitect';
+import { Model, ModelArray, ModelConstant, ModelCustom, ModelObject, ModelOr, ModelPrimitive, Type } from './types';
+
+export { typeArchitect };
+
+export type { Model, ModelArray, ModelConstant, ModelCustom, ModelObject, ModelOr, ModelPrimitive, Type };

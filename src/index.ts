ce1bcc94b7b9140883645011c8ddbdeb2e661c85
Nicolas Assouad <nicolas@assouad.com>
2022-09-20 18:41:28 +0200

    export schema type

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import { number } from './number';
 import { object } from './object';
 import { optional } from './optional';
 import { or } from './or';
+import type { Schema } from './schema';
 import { string } from './string';
 import type { TypeOf } from './TypeOf';
 import { undefinedSchema } from './undefinedSchema';
@@ -13,4 +14,4 @@ import { value } from './value';
 
 export { array, boolean, custom, date, number, object, optional, or, string, undefinedSchema as undefined, value };
 
-export type { TypeOf };
+export type { Schema, TypeOf };
